af0b92fb1f2209eec890840aa93a1877f6ca47d1
野本佳裕 <jiayuyeben@gmail.com>
2018-09-08 12:58:11 +0900

    Edit controller for defining method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,12 @@
 class CommentsController < ApplicationController
 
   def create
+    @comments = Comment.create(id: comment_params[:id], user_id: current_user.id, prototype_id: comment_params[:prototype_id], captured_image_id: comment_params[:captured_image_id], text: comment_params[:text])
+  end
+
+  private
+  def comment_params
+  	params.permit(:id, :prototype_id, :captured_image_id, :text);
   end
 
 end
