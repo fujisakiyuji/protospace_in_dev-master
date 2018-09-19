96c3d403e701ddce0c965aaf2d2aac36c6c7dc10
Riku Taneda <riku.program@gmail.com>
2018-09-19 20:26:56 +0900

    def update and destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,29 @@ class CommentsController < ApplicationController
     end
   end
 
+  def update
+   @comment = Comment.find(params[:id])
+   if @comment.update(comment_params)
+     respond_to do |format|
+       format.html { redirect_to prototype_path(params[:prototype_id])}
+       format.json {render json: @comment}
+     end
+   end
+ end
+
+
+ def destroy
+   @comment = Comment.find(params[:id])
+   if @comment.user_id == current_user.id
+     if @comment.destroy
+       respond_to do |format|
+         format.html { redirect_to prototype_path(params[:prototype_id])}
+         format.json {render json: @comment}
+       end
+     end
+   end
+ end
+
   private
   def comment_params
   	params.permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
